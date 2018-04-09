1e49accb1742de42aed54774f0830609ddc80613
Joe <20873239+CAC27@users.noreply.github.com>
2018-04-09 13:28:24 -0500

    Add Quick Save plugin
    As requested in issue #6 :)

---
+++ plugins/quick_save.js
@@ -0,0 +1,40 @@
+const Plugin = require('../plugin');
+
+module.exports = new Plugin({
+    name: 'Quick Save',
+    author: 'Joe 🎸#7070',
+    description: 'Use Ctrl+S or Cmd+S to save server, channel, or account settings.',
+    color: 'salmon',
+    id: 'quick_save',
+
+    load: async function() {
+        let hcModules = window.findModules('hasChanges');
+        this._listener = function(e) {
+          if ((window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) && e.keyCode == 83) {
+            e.preventDefault();
+            let types = ['GUILD', 'CHANNEL', 'ACCOUNT', 'GUILD ROLES', 'CHANNEL OVERWRITES'];
+            let hasChanges = false;
+            for (let i in types) {
+                if (hcModules[i] && hcModules[i].hasChanges()) {
+                    hasChanges = true;
+                    //module.exports.log(`saving ${types[i]} settings`);
+                    break;
+                }
+            }
+            if (!hasChanges) {
+                //module.exports.log('No setting changes detected. Aborting.');
+                return;
+            }
+            let saveButton = document.querySelector('.lookFilled-luDKDo.colorGreen-22At8E');
+            if (saveButton)
+                try { saveButton.click(); } catch(err) { module.exports.error(err); }
+            return;
+          }
+        }
+        document.addEventListener("keydown", this._listener, false);
+    },
+    unload: function() {
+        document.removeEventListener("keydown", this._listener);
+        delete this._listener;
+    }
+});
