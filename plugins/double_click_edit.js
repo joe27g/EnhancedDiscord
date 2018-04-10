3a38b4a32c7e88d7449da816bf2beeec8ea16b7b
Joe <20873239+CAC27@users.noreply.github.com>
2018-04-09 21:52:46 -0500

    Add Double-Click Edit plugin

---
+++ plugins/double_click_edit.js
@@ -0,0 +1,75 @@
+const Plugin = require('../plugin');
+
+module.exports = new Plugin({
+    name: 'Double-Click Edit',
+    author: 'Joe 🎸#7070',
+    description: 'Allows you to double-click a message to edit it.',
+    color: '#ff5900',
+    id: 'double_click_edit',
+
+    load: async function() {
+        this._listener = function(e) {
+            if (e.target.className !== 'markup') return;
+            let dots = e.target.previousElementSibling.previousElementSibling;
+            if (dots) dots.click();
+            let messageOpts = document.querySelectorAll('.popouts .option-popout .btn-item');
+            if (!messageOpts) return;
+            let hasEditOption = false;
+            for (let i in messageOpts) {
+                if (messageOpts[i].innerHTML == 'Edit') {
+                    messageOpts[i].click(); hasEditOption = true; break;
+                }
+            }
+            if (!hasEditOption && dots)
+                dots.click(); // hide the menu again
+
+            //console.log('double-clicked: ', e.target, this);
+        }
+        document.addEventListener("dblclick", this._listener, false);
+
+        this._deletePressed = false;
+        document.addEventListener("keydown", function(e) {
+            if (e.keyCode == 46)
+                module.exports._deletePressed = true;
+        });
+        document.addEventListener("keyup", function(e) {
+            if (e.keyCode == 46)
+                module.exports._deletePressed = false;
+        });
+        this._listener2 = function(e) {
+            if (e.target.className !== 'markup' || !module.exports._deletePressed) return;
+
+            let dots = e.target.previousElementSibling.previousElementSibling;
+            if (dots) dots.click();
+            let messageOpts = document.querySelectorAll('.popouts .option-popout .btn-item');
+            if (!messageOpts) return;
+            let hasDeleteOption = false;
+            for (let i in messageOpts) {
+                if (messageOpts[i].innerHTML == 'Delete') {
+                    messageOpts[i].click(); hasDeleteOption = true; break;
+                }
+            }
+            if (!hasDeleteOption && dots)
+                return dots.click(); // hide the menu again
+
+            let confirmationModalButton = document.querySelector('.modal-2LIEKY button[type="submit"].colorRed-3HTNPV');
+            if (confirmationModalButton)
+                confirmationModalButton.click();
+        }
+        document.addEventListener("click", this._listener2);
+    },
+    unload: function() {
+        document.removeEventListener("dblclick", this._listener);
+        delete this._listener;
+        document.removeEventListener("keydown", function(e) {
+            if (e.keyCode == 46)
+                module.exports._deletePressed = true;
+        });
+        document.removeEventListener("keyup", function(e) {
+            if (e.keyCode == 46)
+                module.exports._deletePressed = false;
+        });
+        document.removeEventListener("click", this._listener2);
+        delete this._listener2;
+    }
+});
