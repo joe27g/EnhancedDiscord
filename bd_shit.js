3282f0ade9b243d03d90b49889ffc4c3cd7239be
Zack Rauen <rauenzi@outlook.com>
2019-06-25 10:32:51 -0400

    Fix bd plugin settings

--- bd_shit.js
+++ bd_shit.js
@@ -262,7 +262,7 @@ module.exports = class BDManager {
             this.jqueryElement.onload = resolve;
             document.head.appendChild(this.jqueryElement);
         });
-    
+
         this.observer = new MutationObserver((mutations) => {
             for (let i = 0, mlen = mutations.length; i < mlen; i++) this.fireEvent('observer', mutations[i]);
         });
@@ -292,7 +292,7 @@ module.exports = class BDManager {
         const rawMeta = meta.substring(meta.lastIndexOf('//META') + 6, meta.lastIndexOf('*//'));
         if (meta.indexOf('META') < 0) throw new Error('META was not found.');
         if (!EDApi.testJSON(rawMeta)) throw new Error('META could not be parsed.');
-    
+
         const parsed = JSON.parse(rawMeta);
         if (!parsed.name) throw new Error('META missing name data.');
         return parsed;
@@ -365,21 +365,24 @@ module.exports = class BDManager {
     }
 
     static showSettingsModal(plugin) {
+        const baseModalClasses = EDApi.findModule(m => m.modal && m.inner && !m.sizeMedium) || {modal: "modal-36zFtW", inner: "inner-2VEzy9"};
+        const modalClasses = EDApi.findModuleByProps("sizeMedium") || {modal: "backdrop-1wrmKb", sizeMedium: "sizeMedium-ctncE5", content: "content-2KoCOZ", header: "header-2nhbou", footer: "footer-30ewN8", close: "close-hhyjWJ", inner: "inner-2Z5QZX"};
+        const backdrop = EDApi.findModuleByProps("backdrop") || {backdrop: "backdrop-1wrmKb"};
         const modalHTML = `<div id="bd-settingspane-container" class="theme-dark">
-                <div class="backdrop backdrop-1wrmKB" style="background-color: rgb(0, 0, 0); opacity: 0.85;"></div>
-                <div class="modal modal-1UGdnR" style="opacity: 1;">
-                    <div class="inner-1JeGVc">
-                        <div class="modal-3HD5ck sizeMedium-1fwIF2" style="overflow: hidden;">
-                            <div class="flex-1xMQg5 flex-1O1GKY horizontal-1ae9ci horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG justifyStart-2NDFzi alignCenter-1dQNNs noWrap-3jynv6 header-1R_AjF">
+                <div class="backdrop ${backdrop.backdrop}" style="background-color: rgb(0, 0, 0); opacity: 0.85;"></div>
+                <div class="modal ${baseModalClasses.modal}" style="opacity: 1;">
+                    <div class="${baseModalClasses.inner}">
+                        <div class="${modalClasses.modal} ${modalClasses.sizeMedium}" style="overflow: hidden;">
+                            <div class="flex-1xMQg5 flex-1O1GKY horizontal-1ae9ci horizontal-2EEEnY flex-1O1GKY directionRow-3v3tfG justifyStart-2NDFzi alignCenter-1dQNNs noWrap-3jynv6 ${modalClasses.header}">
                                 <h4 class="title h4-AQvcAz title-3sZWYQ size16-14cGz5 height20-mO2eIN weightSemiBold-NJexzi defaultColor-1_ajX0 defaultMarginh4-2vWMG5 marginReset-236NPn">{{modalTitle}}</h4>
-                                <svg viewBox="0 0 12 12" name="Close" width="18" height="18" class="close-button close-18n9bP flexChild-faoVW3"><g fill="none" fill-rule="evenodd"><path d="M0 0h12v12H0"></path><path class="fill" fill="currentColor" d="M9.5 3.205L8.795 2.5 6 5.295 3.205 2.5l-.705.705L5.295 6 2.5 8.795l.705.705L6 6.705 8.795 9.5l.705-.705L6.705 6"></path></g></svg>
+                                <svg viewBox="0 0 12 12" name="Close" width="18" height="18" class="close-button ${modalClasses.close} flexChild-faoVW3"><g fill="none" fill-rule="evenodd"><path d="M0 0h12v12H0"></path><path class="fill" fill="currentColor" d="M9.5 3.205L8.795 2.5 6 5.295 3.205 2.5l-.705.705L5.295 6 2.5 8.795l.705.705L6 6.705 8.795 9.5l.705-.705L6.705 6"></path></g></svg>
                             </div>
-                            <div class="scrollerWrap-2lJEkd content-2BXhLs scrollerThemed-2oenus themeGhostHairline-DBD-2d">
-                                <div id="{{id}}" class="scroller-2FKFPG inner-3wn6Q5 selectable plugin-settings">
+                            <div class="scrollerWrap-2lJEkd scrollerThemed-2oenus themeGhostHairline-DBD-2d ${modalClasses.content}">
+                                <div id="{{id}}" class="scroller-2FKFPG ${modalClasses.inner} selectable plugin-settings">
 
                                 </div>
                             </div>
-                            <div class="flex-1xMQg5 flex-1O1GKY horizontalReverse-2eTKWD horizontalReverse-3tRjY7 flex-1O1GKY directionRowReverse-m8IjIq justifyStart-2NDFzi alignStretch-DpGPf3 noWrap-3jynv6 footer-2yfCgX" style="flex: 0 0 auto;"><button type="button" class="done-button button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeMedium-1AC_Sl grow-q77ONN"><div class="contents-18-Yxp">Done</div></button></div>
+                            <div class="flex-1xMQg5 flex-1O1GKY horizontalReverse-2eTKWD horizontalReverse-3tRjY7 flex-1O1GKY directionRowReverse-m8IjIq justifyStart-2NDFzi alignStretch-DpGPf3 noWrap-3jynv6 ${modalClasses.footer}" style="flex: 0 0 auto;"><button type="button" class="done-button button-38aScr lookFilled-1Gx00P colorBrand-3pXr91 sizeMedium-1AC_Sl grow-q77ONN"><div class="contents-18-Yxp">Done</div></button></div>
                         </div>
                     </div>
                 </div>
