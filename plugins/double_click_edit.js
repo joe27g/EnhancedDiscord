6ddf8782f61be4e8d68d342336fc0eaeb32cdade
joe27g <20873239+joe27g@users.noreply.github.com>
2019-10-14 12:54:47 -0500

    fix double-click edit

--- plugins/double_click_edit.js
+++ plugins/double_click_edit.js
@@ -1,88 +1,72 @@
 const Plugin = require('../plugin');
-let contM = {}, iteM = {}, buttM = {};
+let contM = {}, iteM = {}, buttM = {}, cM, eM, dM;
 
 module.exports = new Plugin({
     name: 'Double-Click Edit',
     author: 'Joe 🎸#7070',
-    description: 'Allows you to double-click a message to edit it, and hold delete + click to delete it.',
+    description: 'Allows you to double-click a message to edit or hold delete + click to delete.',
     color: '#ff5900',
 
-    _deletePressed: false,
+    deletePressed: false,
     load: async function() {
-        contM = findModule('messageCompact');
+        contM = findModule(m => m.container && m.containerCozy);
         iteM = findModule('itemBase');
         buttM = findModule(m => m.buttonSpacing && m.button);
+        cM = findModule('getChannelId');
+        eM = findModule('startEditMessage');
+        dM = findModule('deleteMessage');
+        if (!cM || !eM || !dM) {
+            return this.error('Aborted loading - Failed to find required modules!');
+        }
 
         document.addEventListener("dblclick", this.editListener, false);
         document.addEventListener("keydown", this.keyDownListener);
-        document.addEventListener("keyup", this.keyUpListener);  
+        document.addEventListener("keyup", this.keyUpListener);
         document.addEventListener("click", this.deleteListener);
     },
     unload: async function() {
         document.removeEventListener("dblclick", this.editListener);
         document.removeEventListener("keydown", this.keyDownListener);
-        document.removeEventListener("keyup", this.keyUpListener); 
+        document.removeEventListener("keyup", this.keyUpListener);
         document.removeEventListener("click", this.deleteListener);
     },
 
     editListener: function(e) {
-        //let messageElem = e.target.closest('.'+contM.container);
-        //if (!messageElem) return;
-
-        let dots = document.querySelector('.'+(buttM.button || 'bite_my_shiny_metal_ass').split(' ').join('.'));
-        if (dots) dots.click();
-
-        /*setTimeout(() => {
-            let messageOpts = document.querySelectorAll('.'+(iteM.item || 'bite_my_shiny_metal_ass').split(' ').join('.'));
-            console.log(iteM.item, messageOpts)
-        }, 69)*/
-        let messageOpts = document.querySelectorAll('.'+(iteM.item || 'bite_my_shiny_metal_ass').split(' ').join('.'));
-        //console.log(iteM.item, messageOpts)
-        if (!messageOpts) return;
-
-        let hasEditOption = false;
-        for (let i in messageOpts) {
-            console.log(messageOpts[i].innerText || messageOpts[i]);
-            if (messageOpts[i].innerText == 'Edit') {
-                messageOpts[i].click(); hasEditOption = true; break;
-            }
+        let messageElem = e.target.closest('.'+contM.container);
+        if (!messageElem) return;
+        let msgObj;
+        try {
+            msgObj = messageElem.__reactInternalInstance$.return.return.memoizedProps.message;
+        } catch(err) {
+            this.error(err);
         }
-        if (!hasEditOption && dots)
-            dots.click(); // hide the menu again
-
-        //console.log('double-clicked: ', e.target, this);
+        if (!msgObj) return;
+        const channelId = cM.getChannelId();
+        if (!channelId) return;
+        return eM.startEditMessage(channelId, msgObj.id, msgObj.content || '');
     },
     deleteListener: function(e) {
-        if (!this._deletePressed) return;
+        if (!this.deletePressed) return;
 
         let messageElem = e.target.closest('.'+contM.container);
         if (!messageElem) return;
-
-        let dots = messageElem.querySelector('.'+(buttM.button || 'bite_my_shiny_metal_ass').split(' ').join('.'));
-        if (dots) dots.click();
-
-        let messageOpts = document.querySelectorAll('.'+iteM.item);
-        if (!messageOpts) return;
-
-        let hasDeleteOption = false;
-        for (let i in messageOpts) {
-            if (messageOpts[i].innerText == 'Delete\n') {
-                messageOpts[i].click(); hasDeleteOption = true; break;
-            }
+        let msgObj;
+        try {
+            msgObj = messageElem.__reactInternalInstance$.return.return.memoizedProps.message;
+        } catch(err) {
+            this.error(err);
         }
-        if (!hasDeleteOption && dots)
-            return dots.click(); // hide the menu again
-
-        let confirmationModalButton = document.querySelector('[class="theme-dark"] > [class^="backdrop-"] + [class^="modal-"] form button[type="submit"]');
-        if (confirmationModalButton)
-            confirmationModalButton.click();
+        if (!msgObj) return;
+        const channelId = cM.getChannelId();
+        if (!channelId) return;
+        return dM.deleteMessage(channelId, msgObj.id);
     },
     keyUpListener: function(e) {
         if (e.keyCode == 46)
-            this._deletePressed = false;
+            this.deletePressed = false;
     },
     keyDownListener: function(e) {
         if (e.keyCode == 46)
-            this._deletePressed = true;
+            this.deletePressed = true;
     }
 });
