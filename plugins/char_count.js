0e951d4a6360e04b0d2bcb8a63af8c451f23f860
Joe <20873239+joe27g@users.noreply.github.com>
2019-12-16 18:32:16 -0600

    Update Char Count with rich chat box compatibility

--- plugins/char_count.js
+++ plugins/char_count.js
@@ -1,6 +1,6 @@
 const Plugin = require('../plugin');
 
-let ml = {}, ce = {}, cta = {}, gc, gci, gcu, cm;
+let ml = {}, cta = {}, ta, gs, em, gc, gci, gcu, cm;
 
 module.exports = new Plugin({
     name: 'Character Count',
@@ -10,16 +10,34 @@ module.exports = new Plugin({
 
     load: async function() {
         ml = window.EDApi.findModule('maxLength');
-        em = window.EDApi.findModule(m => m.checkbox && m.errorMessage);
         cta = window.EDApi.findModule('channelTextArea');
+        ta = window.EDApi.findModuleByDisplayName('ChannelTextArea').prototype;
+        gs = window.EDApi.findModule('getAllSettings');
+        em = window.EDApi.findModule(m => m.checkbox && m.errorMessage);
         gc = window.EDApi.findModule('getChannel');
         gci = window.EDApi.findModule('getChannelId');
         gcu = window.EDApi.findModule('getCurrentUser');
         cm = window.EDApi.findModule('createBotMessage');
 
-        document.addEventListener("input", this.inputListener);
-        document.addEventListener("keydown", this.keydownListener);
-        findModule('dispatch').subscribe("MESSAGE_CREATE", this.msgListener);
+        window.EDApi.monkeyPatch(ta, 'render', {after: this.onRender, silent: true});
+        window.EDApi.findModule('dispatch').subscribe("MESSAGE_CREATE", this.msgListener);
+    },
+
+    onRender: function(b) {
+        const ctaElem = document.querySelector('.' + cta.channelTextArea);
+        if (!ctaElem) return;
+        if (!gs.useRichChatTextBox) return module.exports.inputListener(ctaElem); // legacy support
+        const txt = b.thisObject.props.textValue;
+        const parent = ctaElem.parentElement;
+        let charCountElem = parent.querySelector('.' + ml.maxLength);
+        if (!charCountElem) {
+            charCountElem = document.createElement('div');
+            charCountElem.style = "bottom:3px;right:5px;";
+            parent.appendChild(charCountElem);
+        }
+        const len = (txt || '').trim().length;
+        charCountElem.innerHTML = len + '/2000';
+        charCountElem.className = `ed_char_count ${ml.maxLength}${len > 2000 ? ' '+em.errorMessage : ''}`;
     },
 
     msgListener: function(event) {
@@ -29,11 +47,12 @@ module.exports = new Plugin({
         charCounters.forEach(cc => cc.remove());
     },
 
-    inputListener: function(e) {
-        const ctaElem = e.target.closest('.' + cta.channelTextArea);
-        if (!ctaElem) return;
-        const parent = ctaElem.parentElement;
-        if (!parent || !ctaElem.className || !ctaElem.className.startsWith('channelTextArea-')) return;
+    inputListener: function(elem) {
+        if (!elem) return;
+        const taElem = elem.querySelector('textarea');
+        if (!taElem) return;
+        const parent = elem.parentElement;
+        if (!parent || !elem.className || !elem.className.includes(cta.channelTextArea)) return;
         let charCountElem = parent.querySelector('.' + ml.maxLength);
         if (!charCountElem) {
             charCountElem = document.createElement('div');
@@ -41,9 +60,9 @@ module.exports = new Plugin({
             parent.appendChild(charCountElem);
         }
         const chan = gc.getChannel(gci.getChannelId());
-        let len = (e.target.value || '').trim().length;
-        if (chan && e.target.value) {
-            const msgObj = cm.parse(chan, e.target.value);
+        let len = (taElem.value || '').trim().length;
+        if (chan && taElem.value) {
+            const msgObj = cm.parse(chan, taElem.value);
             if (msgObj && msgObj.content)
                 len = msgObj.content.trim().length;
         }
@@ -51,14 +70,8 @@ module.exports = new Plugin({
         charCountElem.className = `ed_char_count ${ml.maxLength}${len > 2000 ? ' '+em.errorMessage : ''}`;
     },
 
-    keydownListener: function(e) {
-        if ((e.keyCode || e.which) === 13) // update char count when enter is pressed. used for autocomplete.
-            return module.exports.inputListener(e);
-    },
-
     unload: function() {
-        document.removeEventListener("input", this.inputListener);
-        document.removeEventListener("keydown", this.keydownListener);
-        findModule('dispatch').unsubscribe("MESSAGE_CREATE", this.msgListener);
+        ta.render.unpatch();
+        window.EDApi.findModule('dispatch').unsubscribe("MESSAGE_CREATE", this.msgListener);
     }
 });
