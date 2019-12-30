36ac1571c80a83de7bb247930d57341228fabdd7
Jakub S <34782021+jakuski@users.noreply.github.com>
2019-12-30 12:12:18 +0000

    SMALL.

--- plugins/ed_settings.js
+++ plugins/ed_settings.js
@@ -12,6 +12,11 @@ const EDSettings = {
     description: 'Adds an EnhancedDiscord tab in user settings.',
 	color: 'darkred',
 	async load () {
+		const discordConstants = EDApi.findModule("API_HOST");
+		const UserSettings = getComponentFromFluxContainer(
+			EDApi.findModule('getUserSettingsSections').default
+		);
+
         if (!window.ED.classMaps) {
             window.ED.classMaps = {};
 		}
@@ -20,32 +25,77 @@ const EDSettings = {
 			window.ED.discordComponents = {};
 		}
 		
-		this.initClassMaps(window.ED.classMaps);
-		this.initDiscordComponents(window.ED.discordComponents);
-
-		const UserSettings = getComponentFromFluxContainer(
-			EDApi.findModule('getUserSettingsSections').default
-		);
+		this._initClassMaps(window.ED.classMaps);
+		this._initDiscordComponents(window.ED.discordComponents);
+		this.settingsSections = this._getDefaultSections(); // Easily allow plugins to add in their own sections if need be.
 
 		this.unpatch = EDApi.monkeyPatch(
 			UserSettings.prototype,
 			"generateSections",
 			data => {
 				const sections = data.originalMethod.call(data.thisObject);
+				const devIndex = this._getDevIndex(sections, discordConstants);
 
-				sections.push({
-					section: "HEADER",
-					label: "YOU'RE GAY"
-				})
+				sections.splice(devIndex + 2, 0, ...this.settingsSections);
+
+				console.log(sections)
 
 				return sections;
 			}
 		)
 	},
 	unload () {
-		this.unpatch();
+		if (this.unpatch && typeof this.unpatch === "function") this.unpatch();
 	},
-	initClassMaps(obj) {
+	_getDevIndex(sections, constants) {
+		const indexOf = sections.indexOf(
+			sections.find(sect => sect.section === constants.UserSettingsSections.DEVELOPER_OPTIONS)
+		);
+
+		if (indexOf !== -1) return indexOf;
+		else return 28; // Hardcoded index fallback incase Discord mess with something
+	},
+	_getDefaultSections() {
+		/*
+
+		For future reference:
+
+		normal sections / pages
+			section: [string] an id string of some sort, must be unique.
+			label: [string] self-explanatory
+			element: [react-renderable (either a funuction or a class)] the page that will be rendered on the right when the section is clicked
+			color: [optional] [string (hex)] a colour to be applied to the section (see the log out / discord nitro btn)
+			onClick: [optional] [function] a function to be executed whenever the element is clicked
+
+		special sections
+			headers
+				section: "HEADER"
+				label: [string]
+			divider
+				section: "DIVIDER"
+			custom element
+				section: "CUSTOM"
+				element: [react-renderable]
+
+		All sections regardless of type can have a optional .predicate property which should be a fn returning a boolean as to whether to show the section or not
+
+		*/
+		return [{
+			section: "HEADER",
+			label: "EDR"
+		},{
+			section: "ED/Plugins",
+			label: "Plugins",
+			element: this.components.PluginsPage
+		},{
+			section: "ED/Settings",
+			label: "Settings",
+			element: this.components.SettingsPage
+		},{
+			section: "DIVIDER"
+		}];
+	},
+	_initClassMaps(obj) {
         const divM = EDApi.findModule(m => m.divider && Object.keys(m).length === 1)
         obj.headers = EDApi.findModule('defaultMarginh2');
         obj.margins = EDApi.findModule('marginBottom8');
@@ -56,21 +106,28 @@ const EDSettings = {
         obj.alignment = EDApi.findModule('horizontalReverse');
         obj.description = EDApi.findModule('formText');
 	},
-	initDiscordComponents(obj) {
-			obj.Textbox = EDApi.findModuleByDisplayName("TextInput"),
-			obj.Select = EDApi.findModuleByDisplayName("SelectTempWrapper"),
-			obj.Switch = EDApi.findModuleByDisplayName("SwitchItem"),
-			obj.RadioGroup = EDApi.findModuleByDisplayName("RadioGroup"),
-			obj.Divider = EDApi.findModuleByDisplayName("FormDivider"), // FIX
-			obj.Title = EDApi.findModuleByDisplayName("FormTitle"),
-			obj.Text = EDApi.findModuleByDisplayName("FormText"),
-			obj.Icon = EDApi.findModuleByDisplayName("Icon"),
-			obj.LoadingSpinner = EDApi.findModuleByDisplayName("Spinner"),
-			obj.Card = EDApi.findModuleByDisplayName("FormNotice"),
-			obj.Flex = EDApi.findModuleByDisplayName("Flex")
+	_initDiscordComponents(obj) {
+		const e = getReact(); 
+
+		obj.Textbox = EDApi.findModuleByDisplayName("TextInput");
+		obj.Select = EDApi.findModuleByDisplayName("SelectTempWrapper");
+		obj.Switch = EDApi.findModuleByDisplayName("SwitchItem");
+		obj.RadioGroup = EDApi.findModuleByDisplayName("RadioGroup");
+		obj.Title = EDApi.findModuleByDisplayName("FormTitle");
+		obj.Text = EDApi.findModuleByDisplayName("FormText");
+		obj.Icon = EDApi.findModuleByDisplayName("Icon");
+		obj.LoadingSpinner = EDApi.findModuleByDisplayName("Spinner");
+		obj.Card = EDApi.findModuleByDisplayName("FormNotice");
+		obj.Flex = EDApi.findModuleByDisplayName("Flex");
+		obj.Switch = EDApi.findModuleByDisplayName("Switch");
+		
+		obj.Divider = props => {
+			props.className = props.className ? props.className + " " + ED.classMaps.divider : ED.classMaps.divider
+			return e("div", Object.assign({}, props))
+		}
 	},
 	components: {
-		PluginPage () {
+		PluginsPage () {
 			const e = getReact(); 
 			return e("div")
 		},
