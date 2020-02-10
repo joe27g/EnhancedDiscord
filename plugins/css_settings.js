954816a7660d1e04d5074f0a9d82ef78f021bff8
Joe G <20873239+joe27g@users.noreply.github.com>
2020-02-10 04:59:21 -0600

    update css loader & css settings [UNFINISHED] for BRED

--- plugins/css_settings.js
+++ plugins/css_settings.js
@@ -1,6 +1,10 @@
 const Plugin = require('../plugin');
-const path = window.require('path');
-const fs = window.require('fs');
+const fs = require('fs');
+const path = require('path');
+const propMap = {
+    bg_color: 'bg',
+    bg_opacity: 'bg-overlay'
+}
 
 module.exports = new Plugin({
     name: 'CSS Settings',
@@ -8,148 +12,153 @@ module.exports = new Plugin({
     description: 'Allows you to modify options for the default theme.',
     color: 'blue',
 
-    config: {},
-    load: async function() {
-        // yeet
-    },
-    unload: function() {
-        // yeet
-    },
-    generateSettings: function() {
-        if (!ED.plugins.css_loader) return;
-        const d = ED.classMaps.description;
-        const b = ED.classMaps.buttons;
-        const id = EDApi.findModule('inputDefault');
-        const m = EDApi.findModule('marginTop8');
-        if (!window.customCss || !window.customCss.innerHTML) return;
+    load: () => {},
+    unload: () => {},
 
-        const mm = window.customCss.innerHTML.match(/--bg: url\(([^)]+)\)/);
-        const overlayM = window.customCss.innerHTML.match(/--bg-overlay: ([^;]+);/);
-        if (!mm || !mm[1]) return;
+    customLoad: function(prop) {
+        const st = ED.plugins.css_loader.settings;
+        if (!st || !st.path) return;
+        const cssPath = path.isAbsolute(st.path) ? st.path : path.join(process.env.injDir, st.path);
 
-        return `<div class="${d.description} ${d.modeDefault}">
-Background Image<br>
-Must be a remote URL, not a local file.
-</div>
-<input type="text" class="${id.inputDefault}" value="${mm[1]}" maxlength="2000" placeholder="https://i.imgur.com/ybRUHPc.jpg" id="custom-bg-url">
-<button type="button" id="save-bg-url" class="${b.button} ${b.lookFilled} ${b.colorBrand} ${m.marginTop8} ${m.marginBottom8}" style="height:24px;margin-right:10px;">
-<div class="${b.contents}">Save</div>
-</button>
-<div class="${d.description} ${d.modeDefault}">
-Background Overlay<br>
-Used to darken the background by default. Change the alpha value (default 0.8) to change the background brightness.
-</div>
-<div style="height:24px;width:24px;display:inline-block;margin:5px;border-radius:5px;background-image:url(&quot;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='2' height='2'%3E%3Cpath d='M1,0H0V1H2V2H1' fill='lightgrey'/%3E%3C/svg%3E&quot;), url(&quot;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='1' height='1'%3E%3Cpath d='M1,0H0V1H2V2H1' fill='white'/%3E%3C/svg%3E&quot;);background-size:40%;vertical-align:top;">
-<div id="preview-bg-overlay" style="width:100%;height:100%;border-radius:5px;background-color:${overlayM[1]}"></div>
-</div>
-<input type="text" class="${id.inputDefault}" value="${overlayM[1]}" placeholder="rgba(0, 0, 0, 0.8)" id="custom-bg-overlay" style="width:auto;">
-<button type="button" id="save-bg-overlay" class="${b.button} ${b.lookFilled} ${b.colorBrand} ${m.marginTop8} ${m.marginBottom8}" style="height:24px;display:inline-block;">
-<div class="${b.contents}">Save</div>
-</button>`;
-    },
-    settingListeners: [
-        {
-            el: '#save-bg-url',
-            type: 'click',
-            eHandler: function() {
-                //console.log(this, e.target);
-                const pathInput = document.getElementById('custom-bg-url');
-                if (!pathInput) return;
-                const cont = this.firstElementChild;
-                if (pathInput.value && !pathInput.value.startsWith('https://') && !pathInput.value.startsWith('http://')) {
-                    cont.innerHTML = 'Invalid URL.';
-                    setTimeout(() => {
-                        try { cont.innerHTML = 'Save'; } catch(err){/*do nothing*/}
-                    }, 3000);
-                    return;
+        let content;
+        try {
+            content = fs.readFileSync(cssPath, 'utf-8');
+        } catch (e) {
+            this.error(e);
+            return null;
+        }
+
+        const lines = content.split(/[\r\n][\r\n]?/).filter(l => l);
+        for (const i in lines) {
+            if (lines[i] && lines[i].match(/^\s*(?:\/\*)?\s*--/)) {
+                const reg = new RegExp(`--${propMap[prop] || prop}: ([^;]+);`);
+                const matches = lines[i].match(reg);
+                if (!matches) continue;
+                const raw = matches[1];
+                if (!raw) continue;
+                console.log(prop, raw);
+
+                if (prop === 'bg' && raw.startsWith('url(')) {
+                    return raw.substring(4, raw.length - 1)
+                } else if (prop === 'bg') {
+                    return null;
                 }
-                const newPath = pathInput.value;
-                const cl = ED.plugins.css_loader;
-                const st = cl.settings;
-                if (!st || !st.path) return;
-                const p = path.join(process.env.injDir, cl.config.path.parse(st.path));
-                fs.readFile(p, 'utf-8', (err, content) => {
-                    if (err) return module.exports.error(err);
-                    const lines = content.split(/\r\n/g).filter(l => l);
-                    let changed = false;
-                    for (const i in lines) {
-                        if (lines[i].indexOf('--bg:') !== -1) {
-                            lines[i] = lines[i].replace(/\/\*(\s+)--bg/, '$1--bg');
-                            lines[i] = lines[i].replace(/--bg:\s+.*;/, `--bg: url(${newPath});`);
-                            changed = true;
-                        }
-                    }
-                    if (changed) {
-                        fs.writeFile(p, lines.join('\n'), err => {
-                            if (err) return module.exports.error(err);
-                            cont.innerHTML = 'Saved.';
-                            setTimeout(() => {
-                                try { cont.innerHTML = 'Save'; } catch(err){/*do nothing*/}
-                            }, 3000);
-                            return;
-                        })
-                    } else {
-                        cont.innerHTML = 'Already saved.';
-                        setTimeout(() => {
-                            try { cont.innerHTML = 'Save'; } catch(err){/*do nothing*/}
-                        }, 3000);
-                        return;
-                    }
-                });
-            }
-        },
-        {
-            el: '#custom-bg-overlay',
-            type: 'input',
-            eHandler: function() {
-                const box = document.getElementById('preview-bg-overlay');
-                if (box && this.value)
-                    box.style['background-color'] = this.value;
-            }
-        },
-        {
-            el: '#save-bg-overlay',
-            type: 'click',
-            eHandler: function() {
-                //console.log(this, e.target);
-                const input = document.getElementById('custom-bg-overlay');
-                if (!input) return;
-                const cont = this.firstElementChild;
-
-                const cl = ED.plugins.css_loader;
-                const st = cl.settings;
-                if (!st || !st.path) return;
-                const p = path.join(process.env.injDir, cl.config.path.parse(st.path));
-                fs.readFile(p, 'utf-8', (err, content) => {
-                    if (err) return module.exports.error(err);
-                    const lines = content.split(/\r\n/g).filter(l => l);
-                    let changed = false;
-                    for (const i in lines) {
-                        if (lines[i].indexOf('--bg-overlay:') !== -1) {
-                            lines[i] = lines[i].replace(/\/\*(\s+)--bg-overlay/, '$1--bg-overlay');
-                            lines[i] = lines[i].replace(/--bg-overlay:\s+.*;/, `--bg-overlay: ${input.value};`);
-                            changed = true;
-                        }
-                    }
-                    if (changed) {
-                        fs.writeFile(p, lines.join('\n'), err => {
-                            if (err) return module.exports.error(err);
-                            cont.innerHTML = 'Saved.';
-                            setTimeout(() => {
-                                try { cont.innerHTML = 'Save'; } catch(err){/*do nothing*/}
-                            }, 3000);
-                            return;
-                        })
-                    } else {
-                        cont.innerHTML = 'Already saved.';
-                        setTimeout(() => {
-                            try { cont.innerHTML = 'Save'; } catch(err){/*do nothing*/}
-                        }, 3000);
-                        return;
-                    }
-                });
+                if (prop === 'bg_color' && !raw.startsWith('url(')) {
+                    return raw;
+                } else if (prop === 'bg_color') {
+                    return null;
+                }
+                if (prop == 'bg_opacity' && raw.startsWith('rgba(')) {
+                    const str = raw.substring(5, raw.length - 1).split(',').pop();
+                    if (!str) return null;
+                    return 100 - Math.round(parseFloat(str)*100);
+                } else if (prop == 'bg_opacity') {
+                    return 100;
+                }
             }
         }
-    ]
+    },
+    customSave: function(prop, data) {
+        if (this._preview) console.log(this._preview);
+        const updateObj = {};
+        switch (prop) {
+            case 'bg':
+                if (data) {
+                    if (!data.startsWith('https://') && !data.startsWith('http://'))
+                        EDApi.showToast('Warning: This location probably won\'t work - it needs to be a remote URL, not a local file.')
+                    updateObj.bg = `url(${data})`;
+                } else {
+                    updateObj.bg = 'transparent'
+                }
+                break;
+            case 'bg_color':
+                updateObj.bg = data || 'transparent';
+                updateObj['bg-overlay'] = 'transparent';
+                break;
+            case 'bg_opacity':
+                updateObj['bg-overlay'] = `rgba(0, 0, 0, ${(100 - data) / 100.0})`;
+                break;
+        }
+        const regexObj = {};
+        for (const varName in updateObj) {
+            regexObj[varName] = new RegExp(`--${varName}: ([^;]+);`)
+        }
 
+        const st = ED.plugins.css_loader.settings;
+        if (!st || !st.path) return;
+        const cssPath = path.isAbsolute(st.path) ? st.path : path.join(process.env.injDir, st.path);
+
+        fs.readFile(cssPath, 'utf-8', (err, content) => {
+            if (err) return module.exports.error(err);
+            const lines = content.split(/\r\n/g).filter(l => l);
+            let changed = false;
+            for (const i in lines) {
+                for (const varName in regexObj) {
+                    if (regexObj[varName].test(lines[i])) {
+                        lines[i] = lines[i].replace(regexObj[varName], `--${varName}: ${updateObj[varName]};`);
+                        changed = true;
+                    }
+                }
+            }
+            if (changed) {
+                fs.writeFile(cssPath, lines.join('\n'), err => {
+                    if (err) {
+                        EDApi.showToast('Error saving: '+err);
+                        return module.exports.error(err);
+                    }
+                    EDApi.showToast('Saved.');
+                    return;
+                })
+            } else {
+                return EDApi.showToast('Already saved.');
+            }
+        });
+    },
+
+    generateSettings: function() {
+        if (!ED.plugins.css_loader || !ED.customCss || !ED.customCss.innerHTML || !ED.customCss.innerHTML.includes("EnhancedDiscord Theme")) return;
+
+        return [{
+            type: "input:text",
+            configName: "bg",
+            title: "Background Image",
+            desc: "Must be a remote URL, not a local file."
+        },{
+            type: "input:slider",
+            configName: "bg_opacity",
+            title: "Background Opacity",
+            desc: "Opacity of the background image underneath other elements. Lowering it increases readability.",
+            defaultValue: 20,
+            highlightDefaultValue: true,
+            markers: [
+                0,10,20,30,40,50,60,69,80,90,100
+            ],
+            stickToMarkers: true,
+            minValue: 0,
+            maxValue: 100,
+        }, this._preview = EDApi.React.createElement('div', {
+            style: {
+                height: 24,
+                width: 24,
+                display: 'inline-block',
+                margin: '5px',
+                borderRadius: '5px',
+                backgroundImage: "url(data:image/svg+xml;base64,PHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScyJyBoZWlnaHQ9JzInPjxwYXRoIGQ9J00xLDBIMFYxSDJWMkgxJyBmaWxsPSdsaWdodGdyZXknLz48L3N2Zz4iKSwgdXJsKCJkYXRhOmltYWdlL3N2Zyt4bWwsPHN2ZyB4bWxucz0naHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmcnIHdpZHRoPScxJyBoZWlnaHQ9JzEnPjxwYXRoIGQ9J00xLDBIMFYxSDJWMkgxJyBmaWxsPSd3aGl0ZScvPjwvc3ZnPg==)",
+                backgroundSize: '40%',
+                verticalAlign: 'top'
+            }
+        }, EDApi.React.createElement('div', {
+            style: {
+                width: '100%',
+                height: '100%',
+                borderRadius: '5px',
+                backgroundColor: this.customLoad('bg_color')
+            }
+        })), {
+            type: "input:text",
+            configName: "bg_color",
+            title: "Background Color",
+            desc: "Set your background to a simple color rather than an image. See the list of [valid css colors](https://developer.mozilla.org/en-US/docs/Web/CSS/color_value)."
+        }];
+    }
 });
