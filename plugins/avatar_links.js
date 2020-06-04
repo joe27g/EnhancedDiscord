d8cd74fc9216fea440fb65bc8770047dbb88a5f2
llyyr <llyyr@users.noreply.github.com>
2020-06-05 04:25:30 +0530

    Fix avatar_links.js

--- plugins/avatar_links.js
+++ plugins/avatar_links.js
@@ -11,7 +11,7 @@ module.exports = new Plugin({
 
     load: async function() {
         ree = this;
-        cm = EDApi.findModule('contextMenu');
+        cm = EDApi.findModule('menu');
         Dispatcher = EDApi.findModule("dispatch");
         ImageResolver = EDApi.findModule("getUserAvatarURL");
         ContextMenuActions = EDApi.findModule("closeContextMenu");
@@ -26,7 +26,7 @@ module.exports = new Plugin({
     checkMenu: async function() {
         // Make sure it's already in the DOM
         await new Promise(r => {setTimeout(r, 5)});
-        const theMenu = document.querySelector('.'+cm.contextMenu);
+        const theMenu = document.querySelector('.'+cm.menu);
         const reactData = theMenu.__reactInternalInstance$;
 
         let label = "";
@@ -39,19 +39,17 @@ module.exports = new Plugin({
             reactData.return.return &&
             reactData.return.return.return &&
             reactData.return.return.return.return &&
-            reactData.return.return.return.return.memoizedProps &&
-            reactData.return.return.return.return.memoizedProps.user &&
-            reactData.return.return.return.return.memoizedProps.type &&
-            reactData.return.return.return.return.memoizedProps.type.startsWith("USER_")
+            reactData.return.return.return.return.return &&
+            reactData.return.return.return.return.return.memoizedProps &&
+            reactData.return.return.return.return.return.memoizedProps.user
         ) {
-            props = reactData.return.return.return.return.memoizedProps;
+            props = reactData.return.return.return.return.return.memoizedProps;
             label = "Copy Avatar URL";
             const user = props.user;
             const imageType = ImageResolver.hasAnimatedAvatar(user) ? "gif" : "png";
 
-            // Internal module maxes at 1024 hardcoded, so do that and change to 2048.
-            url = ImageResolver.getUserAvatarURL(user, imageType, 1024).replace("size=1024", "size=2048");
-
+            // Internal module maxes at 1024 hardcoded, so do that and change to 4096.
+            url = ImageResolver.getUserAvatarURL(user, imageType, 1024).replace("size=1024", "size=4096");
             // For default avatars
             if (!url.startsWith("http") && url.startsWith("/assets"))
                 url = `https://discordapp.com${url}`;
@@ -60,16 +58,16 @@ module.exports = new Plugin({
         // For guilds
         if (
             reactData.return &&
-            reactData.return.memoizedProps &&
-            reactData.return.memoizedProps.guild &&
-            reactData.return.memoizedProps.type == "GUILD_ICON_BAR"
+            reactData.return.return &&
+            reactData.return.return.memoizedProps &&
+            reactData.return.return.memoizedProps.guild
         ) {
-            props = reactData.return.memoizedProps;
+            props = reactData.return.return.memoizedProps;
             label = "Copy Icon URL";
             const guild = props.guild;
 
-            // Internal module maxes at 1024 hardcoded, so do that and change to 2048.
-            url = ImageResolver.getGuildIconURL({id: guild.id, icon: guild.icon, size: 1024}).replace("size=1024", "size=2048");
+            // Internal module maxes at 1024 hardcoded, so do that and change to 4096.
+            url = ImageResolver.getGuildIconURL({id: guild.id, icon: guild.icon, size: 1024}).replace("size=1024", "size=4096");
 
             // No way to make it return the animated version, do it manually
             if (ImageResolver.hasAnimatedGuildIcon(guild))
@@ -84,16 +82,15 @@ module.exports = new Plugin({
     },
 
     addMenuItem: function(imageURL, text, menu) {
-        const cmGroups = document.getElementsByClassName(cm.itemGroup);
+        const cmGroups = document.getElementsByClassName(cm.scroller);
         if (!cmGroups || cmGroups.length == 0) return;
 
         const newCmItem = document.createElement("div");
-        newCmItem.className = cm.item+' '+cm.clickable;
+        newCmItem.className = cm.item+' '+cm.labelContainer+' '+cm.colorDefault;
         const newCmItemContent = document.createElement("div");
         newCmItemContent.className = cm.label;
         newCmItemContent.innerHTML = text;
         newCmItem.appendChild(newCmItemContent);
-
         const lastGroup = cmGroups[cmGroups.length-1];
         lastGroup.appendChild(newCmItem);
 
